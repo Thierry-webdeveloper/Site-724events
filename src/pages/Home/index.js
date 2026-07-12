8fcea8202f73a803990e4dfd9abadea253a71896
Thierry <thierry.pollastri@gmail.com>
2026-07-12 09:45:22 +0200

    Title: Make last event card clickable with details modal
    Description:
    La vignette "dernière prestation" du footer affichait un curseur
    pointer (CSS global .EventCard) sans qu'aucun clic ne produise
    d'effet, aucun onClick n'étant fourni.
    
    Réutilisation du pattern Modal + ModalEvent déjà en place dans
    Events/index.js : la vignette ouvre désormais une modale de détail
    au clic (titre, période, description, participants, prestations),
    identique au comportement des cartes de la grille "Nos réalisations".
    
    last (issu de data.events) dispose de tous les champs requis par
    ModalEvent, contrairement aux événements "focus" du Slider.
    
    DP6 - durcissement production. Aucune régression : yarn test
    toujours au vert.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,6 +10,7 @@ import Logo from "../../components/Logo";
 import Icon from "../../components/Icon";
 import Form from "../../containers/Form";
 import Modal from "../../containers/Modal";
+import ModalEvent from "../../containers/ModalEvent";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
@@ -116,13 +117,25 @@ const Page = () => {
           <h3>Notre derniére prestation</h3>
           {/* Rendre EventCard que lorsque last existe */}
           {last && (
-            <EventCard
-              imageSrc={last?.cover}
-              title={last?.title}
-              date={new Date(last?.date)}
-              small
-              label="boom"
-            />
+            // <EventCard
+            //   imageSrc={last?.cover}
+            //   title={last?.title}
+            //   date={new Date(last?.date)}
+            //   small
+            //   label="boom"
+            // />
+            <Modal Content={<ModalEvent event={last} />}>
+              {({ setIsOpened }) => (
+                <EventCard
+                  onClick={() => setIsOpened(true)}
+                  imageSrc={last?.cover}
+                  title={last?.title}
+                  date={new Date(last?.date)}
+                  small
+                  label="boom"
+                />
+              )}
+            </Modal>
           )}
         </div>
         <div className="col contact">
