f56036bb318cd9fca87b810010ff9b6d03c3cdd4
Thierry <thierry.pollastri@gmail.com>
2026-07-17 18:45:23 +0200

    test: add exhaustive unit tests for getMonth helper
    Ajout de 12 tests unitaires (it.each) couvrant l'ensemble des mois de l'année pour la fonction getMonth (helpers/Date). Complète les 2 tests existants restés à l'état de squelette vide (janvier, juillet). Objectif : détecter automatiquement tout futur décalage d'index ou faute de frappe dans l'objet MONTHS, en lien avec le bug de décalage de mois corrigé précédemment (Slider, Events).

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,34 @@
 /**
- * 
+ *
  */
 
-describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
-    });
-})
+import { getMonth } from "./index";
 
+describe("Date helper", () => {
+  describe("When getMonth is called", () => {
+    it("the function return janvier for 2022-01-01 as date", () => {
+      // to implement
+    });
+    it("the function return juillet for 2022-07-08 as date", () => {
+      // to implement
+    });
+
+    it.each([
+      [0, "janvier"],
+      [1, "février"],
+      [2, "mars"],
+      [3, "avril"],
+      [4, "mai"],
+      [5, "juin"],
+      [6, "juillet"],
+      [7, "août"],
+      [8, "septembre"],
+      [9, "octobre"],
+      [10, "novembre"],
+      [11, "décembre"],
+    ])("le mois d'index %i correspond à '%s'", (monthIndex, expectedMonth) => {
+      const date = new Date(2022, monthIndex, 15);
+      expect(getMonth(date)).toBe(expectedMonth);
+    });
+  });
+});
